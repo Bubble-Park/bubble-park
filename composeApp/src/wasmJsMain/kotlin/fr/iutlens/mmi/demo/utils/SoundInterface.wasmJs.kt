06b8c89d5de291410ea049afef5f71bf5c7bf839
dubois <dubois.iutlens@gmail.com>
2025-04-22 10:54:45 +0200

    Ajout du son sous wasm/js

--- composeApp/src/wasmJsMain/kotlin/fr/iutlens/mmi/demo/utils/SoundInterface.wasmJs.kt
+++ composeApp/src/wasmJsMain/kotlin/fr/iutlens/mmi/demo/utils/SoundInterface.wasmJs.kt
@@ -1,25 +1,69 @@
 package fr.iutlens.mmi.demo.utils
 
+import fr.iutlens.mmi.demo.Res
+import kotlinx.browser.document
+import kotlinx.dom.appendElement
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.w3c.dom.Audio
+import org.w3c.dom.HTMLAudioElement
+import kotlin.contracts.ExperimentalContracts
+
+@OptIn(ExperimentalResourceApi::class,
+    kotlin.contracts.ExperimentalContracts::class)
 actual open class MusicPlayer actual constructor(
     context: Any?,
     resource: String,
     autoplay: Boolean
 ) {
+    val resId = "music-$resource"
+
+    private fun getPlayerElement(): HTMLAudioElement? {
+        return document.getElementById(resId) as? HTMLAudioElement
+    }
+
+    init {
+        release()
+        document.body?.appendElement("audio") {
+            this as HTMLAudioElement
+            this.id = resId
+            this.src = Res.getUri(resource)
+            this.loop = autoplay
+        }
+        if (autoplay) start()
+    }
+
     actual fun start() {
+        getPlayerElement()?.play()
     }
 
     actual fun pause() {
+        getPlayerElement()?.pause()
     }
 
     actual fun release() {
+        val playerEl = getPlayerElement()
+        playerEl?.pause()
+        playerEl?.remove()
     }
 
     actual fun stop(){
+        release()
     }
 }
 
 actual open class SoundPool actual constructor() {
+
+    val map = mutableMapOf<String, Audio>()
+
+    @OptIn(ExperimentalContracts::class, ExperimentalResourceApi::class)
     actual fun load(context: Any?, res: String) {
+        if(map.containsKey(res)) return
+        map[res] = document.body?.appendElement("audio") {
+            this as HTMLAudioElement
+            this.id = "sound-$res"
+            this.src = Res.getUri(res)
+            this.load()
+        } as Audio
     }
 
     actual fun play(
@@ -30,5 +74,11 @@ actual open class SoundPool actual constructor() {
         loop: Int,
         rate: Float
     ) {
+        (map[resource]?.cloneNode(true) as Audio)?.apply {
+            onended = {remove()}
+            playbackRate = rate.toDouble()
+            this.volume = (leftVolume+rightVolume)/2.0
+            play()
+        }
     }
 }
