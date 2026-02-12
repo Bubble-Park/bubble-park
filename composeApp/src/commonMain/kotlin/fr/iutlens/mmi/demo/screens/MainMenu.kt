162b742e1b8bb70a971b1d62afe3777b4c3920f2
Thomas Seyroles <seytom00@gmail.com>
2026-02-12 11:58:04 +0100

    AMélioration du menu pour inclure le background

--- composeApp/src/commonMain/kotlin/fr/iutlens/mmi/demo/screens/MainMenu.kt
+++ composeApp/src/commonMain/kotlin/fr/iutlens/mmi/demo/screens/MainMenu.kt
@@ -1,5 +1,6 @@
 package fr.iutlens.mmi.demo.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
@@ -8,19 +9,64 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import fr.iutlens.mmi.demo.App
+import fr.iutlens.mmi.demo.Res
+import fr.iutlens.mmi.demo.game.sprite.Sprite
+import fr.iutlens.mmi.demo.menu_background
+import fr.iutlens.mmi.demo.menu_nuages
+import fr.iutlens.mmi.demo.menu_premier_plan
+import fr.iutlens.mmi.demo.menu_premier_plan_up
+import fr.iutlens.mmi.demo.menu_second_plan
+import fr.iutlens.mmi.demo.menu_volcan
+import fr.iutlens.mmi.demo.utils.SpriteSheet
+import org.jetbrains.compose.resources.painterResource
 
 @Composable
 fun MainMenu(onPlayClick: () -> Unit) {
     Box(
         modifier = Modifier
-            .fillMaxSize()
-            .background(Color(0xFF1A1A1A)),
+            .fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
+        Image (
+            painter = painterResource(Res.drawable.menu_background),
+            contentDescription = "Fond du menu",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+        Image (
+            painter = painterResource(Res.drawable.menu_nuages),
+            contentDescription = null,
+            contentScale = ContentScale.FillWidth,
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.TopCenter)
+        )
+        Image (
+            painter = painterResource(Res.drawable.menu_second_plan),
+            contentDescription = null,
+            contentScale = ContentScale.FillWidth,
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter)
+        )
+        Image (
+            painter = painterResource(Res.drawable.menu_premier_plan_up),
+            contentDescription = null,
+            contentScale = ContentScale.FillWidth,
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter)
+        )
+
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
