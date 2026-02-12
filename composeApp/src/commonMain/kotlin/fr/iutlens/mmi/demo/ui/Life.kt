d54da5bcf2f4e99a5aa1944a408cdf78ccf798c7
Thomas Seyroles <seytom00@gmail.com>
2026-02-12 16:14:59 +0100

    ajout de style pour l'affichage des 3 vies

--- composeApp/src/commonMain/kotlin/fr/iutlens/mmi/demo/ui/Life.kt
+++ composeApp/src/commonMain/kotlin/fr/iutlens/mmi/demo/ui/Life.kt
@@ -1,15 +1,15 @@
 package fr.iutlens.mmi.demo.ui
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.unit.dp
 import fr.iutlens.mmi.demo.Res
-import fr.iutlens.mmi.demo.components.Player
 import fr.iutlens.mmi.demo.player_heart
 import fr.iutlens.mmi.demo.player_heart_empty
 import org.jetbrains.compose.resources.painterResource
@@ -20,9 +20,12 @@ import org.jetbrains.compose.resources.painterResource
  */
 @Composable
 fun ShowLife(life: Int) {
-    Row() {
+    Row(modifier = Modifier.padding(16.dp), horizontalArrangement = Arrangement.spacedBy(8.dp)) {
         repeat(3) { index ->
             val isFullHeart = index < life
+            var rotate: Float = 4f
+            if (index == 1) rotate = -8f
+            else if (index == 2) rotate = 6f
             val iconRes = if (isFullHeart) Res.drawable.player_heart else Res.drawable.player_heart_empty
             val iconDesc = if (isFullHeart) "Coeur plein" else "Coeur vide"
             Image(
@@ -30,6 +33,7 @@ fun ShowLife(life: Int) {
                 contentDescription = iconDesc,
                 modifier = Modifier
                     .size(32.dp)
+                    .rotate(rotate)
             )
         }
     }
