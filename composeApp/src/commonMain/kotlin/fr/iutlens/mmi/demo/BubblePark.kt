1a2ada38ea0da53955b57f6872472eee58b973a7
Thomas Seyroles <seytom00@gmail.com>
2026-04-08 14:39:56 +0200

    niveau demarre à 40s et bonus coeur debut de lvl spawn en haut

--- composeApp/src/commonMain/kotlin/fr/iutlens/mmi/demo/BubblePark.kt
+++ composeApp/src/commonMain/kotlin/fr/iutlens/mmi/demo/BubblePark.kt
@@ -151,6 +151,7 @@ class BubblePark : GameData() {
         currentLevelDiff = DifficultyManager.getLevelDifficulty(index + 1)
         spawnRatios = DifficultyManager.getSpawnRatios(index + 1)
         chrono = Chrono((DifficultyConfig.TOTAL_LEVEL_TIME * 1000f).toLong())
+        chrono.pause()
         spawnTimerMs = 0L
         levelElapsedMs = 0L
         bonusTimerMs = 0L
@@ -210,10 +211,12 @@ class BubblePark : GameData() {
         }
 
         if (!isFirstLevel) {
-            findSpawnPoint()?.let { (x, y) ->
-                (game.spriteList as? MutableList<Sprite>)?.add(LifeBonus(x, y, player))
+            findSpawnPoint()?.let { (x, _) ->
+                (game.spriteList as? MutableList<Sprite>)?.add(LifeBonus(x, 0f, player))
             }
         }
+
+        game.paused = true
 
         game.animation(20) {
             checkComboReset()
